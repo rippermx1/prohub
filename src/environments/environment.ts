4ada4468f355306fe271519667e6566978cafb51
Carlos Valdes <valdescarlos17@gmail.com>
2025-06-10 18:33:55 -0400

    fix env variables for vercel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
+// The default development environment also reads from `process.env` so that the
+// same variables used in production builds are available locally when running
+// the Angular CLI.
 export const environment = {
   production: false,
-  supabaseUrl: import.meta.env['NG_APP_SUPABASE_URL'],
-  supabaseAnonKey: import.meta.env['NG_APP_SUPABASE_ANON_KEY'],
+  supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
+  supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'],
 };
