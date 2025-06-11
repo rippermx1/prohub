ad9b93f5185587b47358dadc09f3c5d8cf9cd087
Carlos Valdes <valdescarlos17@gmail.com>
2025-06-11 17:25:58 -0400

    refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -28,7 +28,6 @@ export const routes: Routes = [
   {
     path: 'onboarding',
     loadComponent: () => import('./pages/onboarding-wizard/onboarding-wizard.component').then((m) => m.OnboardingWizardComponent),
-    canActivate: [proAuthGuard]
   },
   {
     path: 'login',
