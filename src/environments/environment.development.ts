ab5d974e25c9551a802893c4395b95b2a0b8be79
Carlos Valdes <valdescarlos17@gmail.com>
2025-06-10 20:29:59 -0400

    re-deploy

--- src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] || import.meta.env.NG_APP_SUPABASE_URL || '',
-  supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] || import.meta.env.NG_APP_SUPABASE_ANON_KEY || '',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // the Angular CLI.
 export const environment = {
   production: true,
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'] || import.meta.env.NG_APP_SUPABASE_URL || '',
-  supabaseAnonKey: process.env['NG_APP_SUPABASE_ANON_KEY'] || import.meta.env.NG_APP_SUPABASE_ANON_KEY || '',
+  supabaseUrl: "https://oecotiuceyuugdvbujjl.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lY290aXVjZXl1dWdkdmJ1ampsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3ODY3ODcsImV4cCI6MjA2MzM2Mjc4N30.pjWa8XqDwSz123WBDd_W0VzfPKGxrMR5tVTBnjYJkCk",
 };
